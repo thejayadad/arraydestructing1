117357f4b2acec978a3a78d5239beafdf51a37bf
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-05 13:59:31 -0400

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// array desctructing practice
+
+let student = ["John", 19, "campus", 2025, true];
+
+// another way to destructure the array:
+
+let [one, two, three, four, five] = student
+
+document.write(two)
+
+
+
